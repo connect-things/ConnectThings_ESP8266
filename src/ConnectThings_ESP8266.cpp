a3f9b458aa7dd567d8e321e05c0cfc12dd4c4be0
Francisco Calaça Xavier <chicocx@gmail.com>
2016-12-10 15:21:12 -0200

    licence

--- src/ConnectThings_ESP8266.cpp
+++ src/ConnectThings_ESP8266.cpp
@@ -1,3 +1,26 @@
+/**
+ * @file ConnectThings_ESP8266.cpp
+ * @brief The implementation of class ESP8266. 
+ * @author Francisco Calaça<chicocx@gmail.com> 
+ * @date 2016.12
+ * 
+ * @par Copyright:
+ * Copyright (c) 2016 Francisco Calaça. \n\n
+ * Based on ITEAD Intelligent Systems Co., Ltd. version: ESP8266.cpp\n
+ * ITEADLIB_Arduino_WeeESP8266\n\n
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of
+ * the License, or (at your option) any later version. \n\n
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+ 
 #include "ConnectThings_ESP8266.h"
 #include "Arduino.h"
 #include "SoftwareSerial.h"
